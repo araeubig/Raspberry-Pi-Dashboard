08574293b33f49587d4a335c9b3f3a37c0814abf
André Räubig <129953354+araeubig@users.noreply.github.com>
2024-11-28 08:01:57 +0100

    Added initial base files for "requirements test"

---
+++ dashboard.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import time
+import datetime
+import psutil
+import socket
+import threading
+import netifaces
+import logging
+from collections import deque
+from PIL import Image, ImageDraw, ImageFont
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.exceptions import InfluxDBError
+
+from display import LCD_1inch69,Touch_1inch69
+
+# Raspberry Pi pin configuration:
+RST    = 27
+DC     = 25
+BL     = 18
+TP_INT = 4
+TP_RST = 17
