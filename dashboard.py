7f4d048204c2a0b1c4429ceb7594d82c8184fae2
André Räubig <129953354+araeubig@users.noreply.github.com>
2024-11-28 09:13:43 +0100

    Reduce requirements for testing clean installation

--- dashboard.py
+++ dashboard.py
@@ -5,12 +5,12 @@ import datetime
 import psutil
 import socket
 import threading
-import netifaces
+# import netifaces
 import logging
 from collections import deque
 from PIL import Image, ImageDraw, ImageFont
-from influxdb_client import InfluxDBClient
-from influxdb_client.client.exceptions import InfluxDBError
+# from influxdb_client import InfluxDBClient
+# from influxdb_client.client.exceptions import InfluxDBError
 
 from display import LCD_1inch69,Touch_1inch69
 
