2ec182f35973b54d6519840f31f0c67d5113c8f3
André Räubig <129953354+araeubig@users.noreply.github.com>
2024-11-28 14:55:02 +0100

    Changed service name

--- create_service.sh
+++ create_service.sh
@@ -6,7 +6,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-SERVICE_NAME="dashboard"
+SERVICE_NAME="raspberry_pi_dashboard"
 RUN_SCRIPT="$(pwd)/run.sh"
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 REMOVE_SCRIPT="$(pwd)/remove_service.sh"
