1792facb336c24e7bb75dcc4953c75ea7236e79c
André Räubig <araeubig@me.com>
2024-12-02 09:08:07 +0100

    Removed type helper

--- dashboard.py
+++ dashboard.py
@@ -7,13 +7,13 @@ import socket
 import subprocess
 import threading
 import gettext
-import netifaces # type: ignore
+import netifaces
 # import signal
 import logging
 from collections import deque
 from PIL import Image, ImageDraw, ImageFont
-from influxdb_client import InfluxDBClient # type: ignore
-from influxdb_client.client.exceptions import InfluxDBError # type: ignore
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.exceptions import InfluxDBError
 
 from display.LCD_TOUCH_Waveshare_1inch69 import LCD_1inch69
 from display.LCD_TOUCH_Waveshare_1inch69 import Touch_1inch69
